46c01bf4059f794d3b7e33bfad3a015ed5aab778
Nazmul Takbir <nazmultakbir98@gmail.com>
2023-03-03 13:46:05 +0600

    synthetic words should not have 'skip_chars'

--- DataManager/SyntheticWordDataset.py
+++ DataManager/SyntheticWordDataset.py
@@ -2,6 +2,7 @@ from torch.utils import data
 from Synthesizers.Words.generate import word_generator
 import numpy as np
 from ImageProcessing.image_processing import adjust_contrast_grey
+from Graphemes.utils import skip_chars
 
 class SyntheticWordDataset(data.Dataset):
     def __init__(self, inp_h=32, inp_w=128, transform=None):
@@ -14,7 +15,11 @@ class SyntheticWordDataset(data.Dataset):
         return 100000
 
     def __getitem__(self, idx):
-        image, word = word_generator(self.inp_h, self.inp_w)
+        while True:
+            image, word = word_generator(self.inp_h, self.inp_w)
+            if not any((c in skip_chars) for c in word):
+                break
+        
         image = np.array(image)
         image = adjust_contrast_grey(image)
 
